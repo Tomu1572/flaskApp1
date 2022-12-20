fdfdf69480d0331a231303bf13a952a21e0dabcb
Tomu1572 <myster.pmysterios@gmail.com>
2022-12-20 13:10:27 +0700

    Form Lab04

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,6 @@ from flask import Flask
 app = Flask(__name__, static_folder='static')
  
 app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = '522fba4559295af5b81785106665ae3b882cf9983ffb5803'
  
 from app import views  # noqa
