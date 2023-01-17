5f2741b6278dd4484283a57306ac61dc9ddd8f94
Tomu1572 <myster.pmysterios@gmail.com>
2023-01-17 13:48:58 +0700

    Lab09-AJAX

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
  
 app = Flask(__name__, static_folder='static')
- 
+
+app.config['JSON_AS_ASCII'] = False 
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = '522fba4559295af5b81785106665ae3b882cf9983ffb5803'
  
